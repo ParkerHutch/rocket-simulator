779074e42b5cc573b3f2a26b85e19aaacaf5735b
unknown <parker.hutch@outlook.com>
2020-12-10 14:12:00 -0500

    cleanup

--- src/rocket/ManeuverCalculator.java
+++ src/rocket/ManeuverCalculator.java
@@ -3,8 +3,6 @@ import world.World;
 
 public class ManeuverCalculator {
 	
-	// TODO add a deltav calculation method?
-	
 	private Rocket rocket;
 	private double groundY;
 	
@@ -12,7 +10,7 @@ public class ManeuverCalculator {
 	
 	/**
 	 * Creates a ManeuverCalculator for a given Rocket and stores a given 
-	 * y-coordinate as the ground's y-coordinate
+	 * y-coordinate as the ground's y-coordinate.
 	 * @param rocket the Rocket to calculate maneuvers for
 	 * @param groundY the y-coordinate of the ground, measured in pixels from
 	 * the top of the screen
@@ -26,7 +24,7 @@ public class ManeuverCalculator {
 
 	/**
 	 * Gets the Rocket which the ManeuverCalculator is calculating maneuvers
-	 * for
+	 * for.
 	 * @return the ManeuverCalculator's Rocket
 	 */
 	public Rocket getRocket() {
@@ -35,7 +33,7 @@ public class ManeuverCalculator {
 
 	/**
 	 * Sets the Rocket which the ManeuverCalculator is calculating maneuvers
-	 * for
+	 * for.
 	 * @param rocket the new Rocket for the ManeuverCalculator
 	 */
 	public void setRocket(Rocket rocket) {
@@ -43,7 +41,7 @@ public class ManeuverCalculator {
 	}
 	
 	/**
-	 * Gets the top y coordinate of the ground
+	 * Gets the top y coordinate of the ground.
 	 * @return the ground's top y coordinate
 	 */
 	public double getGroundY() {
@@ -51,7 +49,7 @@ public class ManeuverCalculator {
 	}
 	
 	/**
-	 * Sets the top y coordinate of the ground
+	 * Sets the top y coordinate of the ground.
 	 * @param groundY the new ground y coordinate
 	 */
 	public void setGroundY(double groundY) {
@@ -60,7 +58,7 @@ public class ManeuverCalculator {
 	
 	/**
 	 * Returns true if the Rocket should fire its engines to land at the
-	 * specified height with 0 velocity
+	 * specified height with 0 velocity.
 	 * @param targetHeight the height the Rocket wishes to achieve 0 velocity
 	 * at
 	 * @return true if the Rocket should fire its engines, false otherwise
@@ -105,36 +103,6 @@ public class ManeuverCalculator {
 	}
 	
 	/**
-	 * Performs the quadratic formula given coefficients for the x^2, x, and
-	 * constant values
-	 * @param a the coefficient of the x^2 component
-	 * @param b the coefficient of the x component
-	 * @param c the coefficient of the constant component
-	 * @return a sorted array of the solutions, with the greater solution first
-	 */
-	public double[] doQuadraticFormula(double a, double b, double c) {
-		// x = (-b +- sqrt(b^2 - 4ac)) / 2a
-		double solution1 = (-b + Math.sqrt((b * b) - 4 * a * c)) / (2 * a);
-		double solution2 = (-b - Math.sqrt((b * b) - 4 * a * c)) / (2 * a);
-		double [] solutions = {solution1, solution2};
-		
-		if (solution1 > solution2) {
-			
-			solutions[0] = solution1;
-			solutions[1] = solution2;
-			
-		} else {
-			
-			solutions[0] = solution2;
-			solutions[1] = solution1;
-			
-		}
-		
-		return solutions;
-		
-	}
-	
-	/**
 	 * Calculates the height that the Rocket should begin burning its engine at 
 	 * full throttle in order to have a y velocity of 0 when it reaches the 
 	 * ground. 
@@ -143,10 +111,6 @@ public class ManeuverCalculator {
 	 */
 	public double calculateBurnHeight(double safetyMargin) {
 		
-		/*
-		double thrustYAccel = (getThrustPower() * 
-				Math.sin(Math.toRadians(getDirection()))) - World.GRAVITY;
-		*/
 		double thrustYAccel = (calculateTotalThrustPower() * 
 				Math.sin(Math.toRadians(rocket.getDirection()))) - World.GRAVITY;
 		if (thrustYAccel < 0) {
