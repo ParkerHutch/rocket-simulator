935deccff3f5935acf62ee80057fb4b92877c6a2
unknown <parker.hutch@outlook.com>
2020-12-10 19:48:34 -0500

    documentation

--- src/rocket/UserControlledRocket.java
+++ src/rocket/UserControlledRocket.java
@@ -1,41 +1,89 @@
 package rocket;
+
+/**
+ * A class used to add user interaction functionality to the Rocket class.
+ */
 public class UserControlledRocket extends Rocket {
 
 	double targetAngle = 90;
 	boolean shouldFireEngines = false;
 	boolean shouldFireRCS = false;
 	
+	/**
+	 * Creates a UserControlledRocket at (x, y) with the given fuel and ground
+	 * y-coordinate.
+	 * @param x the UserControlledRocket's center x-coordinate
+	 * @param y the UserControlledRocket's center y-coordinate
+	 * @param fuel the UserControlledRocket's fuel amount
+	 * @param groundY the ground's top y-coordinate
+	 */
 	public UserControlledRocket(double x, double y, double fuel, double groundY) {
 		
 		super(x, y, fuel, groundY);
 		
 	}
 	
+	/**
+	 * Gets the angle towards which the UserControlledRocket should be rotating.
+	 * @return the target angle
+	 */
 	public double getTargetAngle() {
 		return targetAngle;
 	}
 
+	/**
+	 * Sets the angle towards which the UserControlledRocket should be rotating.
+	 * @param targetAngle the target angle
+	 */
 	public void setTargetAngle(double targetAngle) {
 		this.targetAngle = targetAngle;
 	}
 
+	/**
+	 * Returns whether the UserControlledRocket's engines should currently be
+	 * firing.
+	 * @return whether the UserControlledRocket's engines should be on.
+	 */
 	public boolean shouldFireEngines() {
 		return shouldFireEngines;
 	}
 
+	/**
+	 * Sets whether the UserControlledRocket's should currently be firing.
+	 * @param shouldFireEngines whether the UserControlledRocket's engines 
+	 * should be on.
+	 */
 	public void setShouldFireEngines(boolean shouldFireEngines) {
 		this.shouldFireEngines = shouldFireEngines;
 	}
 
-	
+	/**
+	 * Returns whether the UserControlledRocket's RCS thrusters should
+	 * currently be firing.
+	 * @return whether the UserControlledRocket's RCS thrusters should be
+	 * firing
+	 */
 	public boolean shouldFireRCS() {
 		return shouldFireRCS;
 	}
 
+	/**
+	 * Sets whether the UserControlledRocket's RCS thrusters should
+	 * currently be firing.
+	 * @param shouldFireRCS whether the UserControlledRocket's RCS thrusters 
+	 * should be firing
+	 */
 	public void setShouldFireRCS(boolean shouldFireRCS) {
 		this.shouldFireRCS = shouldFireRCS;
 	}
 
+	/**
+	 * Resets various position, flight, and visual variables so that the 
+	 * UserControlledRocket is ready to begin another simulation.
+	 * @param x the new x-coordinate of the UserControlledRocket
+	 * @param y the new y-coordinate of the UserControlledRocket
+	 * @param fuel the new fuel amount of the UserControlledRocket
+	 */
 	public void reset(double x, double y, double fuel) {
 
 		setDirection(90);
