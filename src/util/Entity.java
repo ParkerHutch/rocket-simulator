82730e69ccd3cca6da655e3d33e1461b0fe43e3f
unknown <parker.hutch@outlook.com>
2020-12-11 15:07:42 -0500

    cleanup and documentation

--- src/util/Entity.java
+++ src/util/Entity.java
@@ -2,6 +2,10 @@ package util;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+/**
+ * A class containing general data for most elements that take part in the 
+ * simulation.
+ */
 public abstract class Entity {
 	
 	private double x; 
@@ -31,9 +35,8 @@ public abstract class Entity {
 		this.y = y;
 	}
 
-
 	/**
-	 * Creates an Entity with arguments for position, size, and color.
+	 * Creates an Entity with arguments for position and color.
 	 * @param x the middle x coordinate of the Entity
 	 * @param y the top y coordinate of the Entity
 	 * @param color the color of the Entity
@@ -44,7 +47,17 @@ public abstract class Entity {
 		this.color = color;
 	}
 	
-	public Entity(double x, double y, Color color, double xOffset, double yOffset) {
+	/**
+	 * Creates an Entity with arguments for position, color, and x and y 
+	 * offsets.
+	 * @param x the middle x coordinate of the Entity
+	 * @param y the top y coordinate of the Entity
+	 * @param color the color of the Entity
+	 * @param xOffset the Entity's x offset
+	 * @param yOffset the Entity's y offset
+	 */
+	public Entity(double x, double y, Color color, double xOffset, 
+					double yOffset) {
 		this.x = x;
 		this.y = y;
 		this.color = color;
@@ -52,7 +65,19 @@ public abstract class Entity {
 		this.yOffset = yOffset;
 	}
 
-	public Entity(double x, double y, double width, double height, Color color, double xOffset, double yOffset) {
+	/**
+	 * Creates an Entity with arguments for position, size, color, and x and y 
+	 * offsets.
+	 * @param x the middle x coordinate of the Entity
+	 * @param y the top y coordinate of the Entity
+	 * @param width the width of the Entity
+	 * @param height the height of the Entity
+	 * @param color the color of the Entity
+	 * @param xOffset the Entity's x offset
+	 * @param yOffset the Entity's y offset
+	 */
+	public Entity(double x, double y, double width, double height, Color color, 
+					double xOffset, double yOffset) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
@@ -62,14 +87,21 @@ public abstract class Entity {
 		this.yOffset = yOffset;
 	}
 
-
-	public Entity(double xOffset, double yOffset, double width, double height) {
+	/**
+	 * Creates an Entity with no x and y position with the given x and y 
+	 * offsets and dimensions.
+	 * @param xOffset the Entity's x offset
+	 * @param yOffset the Entity's y offset
+	 * @param width the Entity's width
+	 * @param height the Entity's height
+	 */
+	public Entity(double xOffset, double yOffset, double width, 
+					double height) {
 		this.xOffset = xOffset;
 		this.yOffset = yOffset;
 		this.width = width;
 		this.height = height;
 	}
-	
 
 	/**
 	 * Draws the Entity on a Canvas
@@ -115,36 +147,66 @@ public abstract class Entity {
 		this.y = y;
 	}
 
-
+	/**
+	 * Gets the width of the Entity.
+	 * @return the Entity's width
+	 */
 	public double getWidth() {
 		return this.width;
 	}
 
+	/**
+	 * Sets the width of the Entity.
+	 * @param width the Entity's new width
+	 */
 	public void setWidth(double width) {
 		this.width = width;
 	}
 
+	/**
+	 * Gets the height of the Entity.
+	 * @return the Entity's height
+	 */
 	public double getHeight() {
 		return this.height;
 	}
 
+	/**
+	 * Sets the height of the Entity.
+	 * @param height the Entity's new height
+	 */
 	public void setHeight(double height) {
 		this.height = height;
 	}
 
-
+	/**
+	 * Gets the Entity's x offset, used when the Entity is drawn.
+	 * @return the Entity's x offset
+	 */
 	public double getxOffset() {
 		return xOffset;
 	}
 
+	/**
+	 * Sets the Entity's x offset, used when the Entity is drawn.
+	 * @param xOffset the Entity's new x offset
+	 */
 	public void setxOffset(double xOffset) {
 		this.xOffset = xOffset;
 	}
 
+	/**
+	 * Gets the Entity's y offset, used when the Entity is drawn.
+	 * @return the Entity's y offset
+	 */
 	public double getyOffset() {
 		return yOffset;
 	}
 
+	/**
+	 * Sets the Entity's y offset, used when the Entity is drawn.
+	 * @param yOffset the Entity's new y offset
+	 */
 	public void setyOffset(double yOffset) {
 		this.yOffset = yOffset;
 	}
@@ -158,7 +220,6 @@ public abstract class Entity {
 		return direction;
 	}
 
-
 	/**
 	 * Sets the angle from the positive x-axis counterclockwise to the top of
 	 * the Entity
@@ -168,7 +229,6 @@ public abstract class Entity {
 		this.direction = direction;
 	}
 
-
 	/**
 	 * Gets the Vector2D object representing the Entity's 2D velocity
 	 * @return
@@ -176,7 +236,6 @@ public abstract class Entity {
 	public Vector2D getVelocity() {
 		return velocity;
 	}
-
 
 	/**
 	 * Sets a new Vector2D object for the Entity's velocity
@@ -186,10 +245,18 @@ public abstract class Entity {
 		this.velocity = velocity;
 	}
 
+	/**
+	 * Gets the Entity's acceleration vector.
+	 * @return the Entity's acceleration
+	 */
 	public Vector2D getAcceleration() {
 		return acceleration;
 	}
 
+	/**
+	 * Sets the Entity's acceleration vector.
+	 * @param acceleration the Entity's new acceleration
+	 */
 	public void setAcceleration(Vector2D acceleration) {
 		this.acceleration = acceleration;
 	}
@@ -220,6 +287,11 @@ public abstract class Entity {
 		
 	}
 	
+	/**
+	 * Updates the Entity's position and velocity by adding the velocity to 
+	 * the position and adding the acceleration to the velocity.
+	 * @param timeElapsed the time since the last update, in seconds
+	 */
 	public void applyForces(double timeElapsed) {
 		
 		applyVelocity(timeElapsed);
@@ -242,14 +314,28 @@ public abstract class Entity {
 		this.color = color;
 	}
 
+	/**
+	 * Returns whether the Entity is currently being drawn on the screen.
+	 * @return whether the Entity is visible
+	 */
 	public boolean isVisible() {
 		return visible;
 	}
 
+	/**
+	 * Sets whether the Entity should be drawn on the screen.
+	 * @param visible whether the Entity should be visible
+	 */
 	public void setVisible(boolean visible) {
 		this.visible = visible;
 	}
 
+	/**
+	 * Aligns this Entity to another Entity by matching the other Entity's 
+	 * position, adding this Entity's offsets to that position, and then 
+	 * matching the other Entity's velocity and direction.
+	 * @param target the Entity to align this Entity with
+	 */
 	public void alignWith(Entity target) {
 
 		setX(target.getX() + getxOffset());
@@ -259,6 +345,12 @@ public abstract class Entity {
 
 	}
 
+	/**
+	 * Aligns the Entity with a point by setting this Entity's position to the
+	 * given point with this Entity's offsets added on.
+	 * @param x the x-coordinate of the point to align with
+	 * @param y the y-coordinate of the point to align with
+	 */
 	public void alignWith(double x, double y) {
 
 		setX(x + getxOffset());
