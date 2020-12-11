835ec2e2e4b7a6bbfa590011158683259b7ac117
unknown <parker.hutch@outlook.com>
2020-12-11 14:55:28 -0500

    cleanup and documentation

--- src/userinterface/VelocityIndicator.java
+++ src/userinterface/VelocityIndicator.java
@@ -8,10 +8,26 @@ import javafx.scene.paint.Color;
 import util.Entity;
 import rocket.Rocket;
 
+/**
+ * An element that shows the user a visual and numerical representation of the
+ * Rocket's velocity's magnitude and sign. The class should be extended to
+ * implement the way the arrow and velocity are displayed as those aspects are
+ * specific to which velocity (horizontal/vertical) is being displayed.
+ */
 public abstract class VelocityIndicator extends Entity {
 	
 	private Rocket rocket;
 	
+	/**
+	 * Creates a VelocityIndicator with the given x and y offsets and 
+	 * dimensions and passes it a Rocket to track the velocity of.
+	 * @param xOffset the VelocityIndicator's x offset
+	 * @param yOffset the VelocityIndicator's y offset
+	 * @param width the VelocityIndicator's width
+	 * @param height the VelocityIndicator's height
+	 * @param rocket the Rocket the VelocityIndicator should track the velocity
+	 * of
+	 */
 	public VelocityIndicator(double xOffset, double yOffset, 
 			double width, double height, Rocket rocket) {
 		
@@ -20,18 +36,39 @@ public abstract class VelocityIndicator extends Entity {
 		
 	}
 
+	/**
+	 * Gets the Rocket that the VelocityIndicator should be tracking.
+	 * @return the VelocityIndicator's Rocket
+	 */
 	public Rocket getRocket() {
 		return rocket;
 	}
 
+	/**
+	 * Sets the Rocket that the VelocityIndicator should be tracking.
+	 * @param rocket the VelocityIndicator's new Rocket
+	 */
 	public void setRocket(Rocket rocket) {
 		this.rocket = rocket;
 	}
 
+	/**
+	 * Draws a velocity arrow within the bounds of the VelocityIndicator
+	 * representing the direction of velocity.
+	 * @param gc the GraphicsContext used to draw the VelocityIndicator
+	 */
     protected abstract void drawVelocityArrow(GraphicsContext gc);
 
+	/**
+	 * Gets a String representing the Rocket's velocity.
+	 * @return the Rocket's velocity text
+	 */
     protected abstract String getVelocityText();
 
+	/**
+	 * Draws the Rocket's velocity text box.
+	 * @param gc the GraphicsContext to draw the VelocityIndicator on
+	 */
     private void drawVelocityText(GraphicsContext gc) {
 		
 		gc.setFill(Color.WHITE);
@@ -40,9 +77,7 @@ public abstract class VelocityIndicator extends Entity {
 		
 		gc.setTextAlign(TextAlignment.CENTER);
 		
-		//gc.setFont(new Font(20)); TODO use tahoma font
-		
-		gc.setTextBaseline(VPos.CENTER); // TODO needed?
+		gc.setTextBaseline(VPos.CENTER); 
 		
         gc.fillText(
             getVelocityText(),
@@ -50,7 +85,11 @@ public abstract class VelocityIndicator extends Entity {
 			getY() + getHeight() / 2);
 
     }
-    
+	
+	/**
+	 * Draws a background box covering the bounds of the VelocityIndicator.
+	 * @param gc the GraphicsContext used to draw the VelocityIndicator
+	 */
     private void drawBackgroundBox(GraphicsContext gc) {
 
         gc.setFill(Color.WHITE);
