57882c257f1174b3892481be729383ca226e52e1
unknown <parker.hutch@outlook.com>
2020-12-11 13:01:33 -0500

    cleanup and documentation

--- src/userinterface/MinimizeMaximizeButton.java
+++ src/userinterface/MinimizeMaximizeButton.java
@@ -2,6 +2,12 @@ package userinterface;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+/**
+ * An element that allows the user to minimize or maximize the UserInterface.
+ * The UserInterface continuously checks the <code>maximized</code> state of
+ * its MinimizeMaximizeButton to determine whether it should be minimized or 
+ * maximized. 
+ */
 public class MinimizeMaximizeButton extends CustomButton {
 	
 	private boolean maximized = true;
@@ -11,6 +17,14 @@ public class MinimizeMaximizeButton extends CustomButton {
 	
 	private Color symbolColor = Color.RED;
 	
+	/**
+	 * Creates a MinimizeMaximizeButton with the given x and y offsets and
+	 * dimensions.
+	 * @param xOffset the MinimizeMaximizeButton's x offset
+	 * @param yOffset the MinimizeMaximizeButton's y offset
+	 * @param width the MinimizeMaximizeButton's width
+	 * @param height the MinimizeMaximizeButton's height
+	 */
 	public MinimizeMaximizeButton(double xOffset, double yOffset, 
 			double width, double height) {
 		super(xOffset, yOffset, width, height);
@@ -20,56 +34,81 @@ public class MinimizeMaximizeButton extends CustomButton {
 	
 	}
 	
+	/**
+	 * Returns whether the MinimizeMaximizeButton has been set to the maximized
+	 * state.
+	 * @return the maximized state of the MinimizeMaximizeButton
+	 */
 	public boolean isMaximized() {
 		return maximized;
 	}
 
+	/**
+	 * Sets whether the MinimizeMaximizeButton has been set to the maximized
+	 * state.
+	 * @param maximized the new maximized state of the MinimizeMaximizeButton
+	 */
 	public void setMaximized(boolean maximized) {
 		this.maximized = maximized;
 	}
 
-	public double getSymbolWidth() {
+	/**
+	 * Gets the width of the minor symbol inside the MinimizeMaximizeButton.
+	 * @return the symbol width
+	 */
+	private double getSymbolWidth() {
 		return symbolWidth;
 	}
 
-	public void setSymbolWidth(double symbolWidth) {
+	/**
+	 * Sets the width of the minor symbol inside the MinimizeMaximizeButton.
+	 * @param symbolWidth the new symbol width
+	 */
+	private void setSymbolWidth(double symbolWidth) {
 		this.symbolWidth = symbolWidth;
 	}
 
-	public double getSymbolHeight() {
+	/**
+	 * Gets the height of the minor symbol inside the MinimizeMaximizeButton.
+	 * @return the symbol height
+	 */
+	private double getSymbolHeight() {
 		return symbolHeight;
 	}
 
-	public void setSymbolHeight(double symbolHeight) {
+	/**
+	 * Sets the height of the minor symbol inside the MinimizeMaximizeButton.
+	 * @param symbolHeight the new symbol height
+	 */
+	private void setSymbolHeight(double symbolHeight) {
 		this.symbolHeight = symbolHeight;
 	}
 
+	/**
+	 * Gets the fill color of the minor symbol inside the 
+	 * MinimizeMaximizeButton.
+	 * @return the symbol color
+	 */
 	public Color getSymbolColor() {
 		return symbolColor;
 	}
 
+	/**
+	 * Sets the fill color of the minor symbol inside the 
+	 * MinimizeMaximizeButton.
+	 * @param symbolColor the new symbol color
+	 */
 	public void setSymbolColor(Color symbolColor) {
 		this.symbolColor = symbolColor;
 	}
 
-	public void drawPlaySymbol(GraphicsContext gc) {
-
-		double[] triangleXPoints = new double[] {
-
-				getX() - getSymbolWidth() / 2, getX() - getSymbolWidth() / 2, getX() + getSymbolWidth() / 2
-
-		};
-
-		double[] triangleYPoints = new double[] {
-
-				getCenterY() + getSymbolHeight() / 2, getCenterY() - getSymbolHeight() / 2, getCenterY()
-
-		};
-
-		gc.fillPolygon(triangleXPoints, triangleYPoints, 3);
-
-	}
-
+	/**
+	 * Draws a triangle symbol to represent whether the button's maximized
+	 * state will be set to true or false on the next click.
+	 * @param gc the GraphicsContext used to draw the MinimizeMaximizeButton
+	 * @param pointedLeft true if the triangle should point left, false if it
+	 * should point right
+	 */
 	public void drawTriangle(GraphicsContext gc, boolean pointedLeft) {
 
 		double [] triangleXPoints;
