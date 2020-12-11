2929d0ac992c95ed6dbe2d6a0270cf9c5d86bbf0
unknown <parker.hutch@outlook.com>
2020-12-11 16:08:53 -0500

    cleanup and documentation

--- src/world/World.java
+++ src/world/World.java
@@ -8,14 +8,16 @@ import rocket.Rocket;
 import rocket.UserControlledRocket;
 import design.ColorPalette;
 
+/**
+ * A class used to encapsulate all the bodies that interact in the simulation.
+ */
 public class World {
 	
 	private ArrayList<Entity> objects = new ArrayList<Entity>();
 	
-	public static final double GRAVITY = 100; // pixels/second (150 is a nice number)
+	public static final double GRAVITY = 100; // pixels/second^2
 	private double groundHeight = 100;
 	private double groundY;
-	private double time = 0; // NOTE: Should this be stored here?
 	
 	private double windowWidth;
 	private double windowHeight;
@@ -31,6 +33,14 @@ public class World {
 
 	World() {}
 	
+	/**
+	 * Creates a World for a window of given dimensions and a given color 
+	 * palette.
+	 * @param windowWidth the width of the window this World occupies
+	 * @param windowHeight the height of the window this World occupies
+	 * @param palette the color palette to use when drawing the World and its
+	 * objects
+	 */
 	public World(double windowWidth, double windowHeight, ColorPalette palette) {
 		
 		this.windowWidth = windowWidth;
@@ -41,96 +51,181 @@ public class World {
 		this.palette = palette;
 	}
 
-	public double getWindowWidth() {
+	/**
+	 * Gets the width of the window this World occupies.
+	 * @return the window width
+	 */
+	private double getWindowWidth() {
 		return windowWidth;
 	}
 
+	/**
+	 * Sets the width of the window this World occupies.
+	 * @param windowWidth the window width
+	 */
 	public void setWindowWidth(double windowWidth) {
 		this.windowWidth = windowWidth;
 	}
 
+	/**
+	 * Gets the height of the window this World occupies.
+	 * @return the window height
+	 */
 	public double getWindowHeight() {
 		return windowHeight;
 	}
 
+	/**
+	 * Sets the height of the window this World occupies.
+	 * @param windowHeight the window height
+	 */
 	public void setWindowHeight(double windowHeight) {
 		this.windowHeight = windowHeight;
 	}
 
+	/**
+	 * Gets the list of Entities associated with this World.
+	 * @return the World's objects
+	 */
 	public ArrayList<Entity> getObjects() {
 		return objects;
 	}
 
+	/**
+	 * Sets the list of Entities associated with this World.
+	 * @param objects the World's new objects
+	 */
 	public void setObjects(ArrayList<Entity> objects) {
 		this.objects = objects;
 	}
 
-	public MountainManager getMountainManager() {
+	/**
+	 * Gets the MountainManager used to handle mountain creation and drawing.
+	 * @return this World's MountainManager
+	 */
+	private MountainManager getMountainManager() {
 		return mountainManager;
 	}
 
+	/**
+	 * Sets the MountainManager used to handle mountain creation and drawing.
+	 * @param mountainManager this World's MountainManager
+	 */
 	public void setMountainManager(MountainManager mountainManager) {
 		this.mountainManager = mountainManager;
 	}
 
+	/**
+	 * Gets the ground's top y-coordinate.
+	 * @return the ground's top y-coordinate
+	 */
 	public double getGroundY() {
 		return groundY;
 	}
 
+	/**
+	 * Sets the ground's top y-coordinate.
+	 * @param groundY the ground's top y-coordinate
+	 */
 	public void setGroundY(double groundY) {
 		this.groundY = groundY;
 	}
 	
+	/**
+	 * Gets the height of the rectangle used to represent the ground. Used for
+	 * drawing purposes.
+	 * @return the ground height
+	 */
 	public double getGroundHeight() {
 		return groundHeight;
 	}
 
+	/**
+	 * Sets the height of the rectangle used to represent the ground.
+	 * @param groundHeight the ground height
+	 */
 	public void setGroundHeight(double groundHeight) {
 		this.groundHeight = groundHeight;
 	}
 
-	public double getTime() {
-		return time;
-	}
-
-	public void setTime(double time) {
-		this.time = time;
-	}
-
+	/**
+	 * Returns true if the world 'camera' is currently following the 
+	 * <code>primaryRocket</code> horizontally, and false otherwise
+	 * @return whether the world camera is centered on the rocket horizontally
+	 */
 	public boolean centerOnRocketHorizontally() {
 		return centerOnRocketHorizontally;
 	}
 
+	/**
+	 * Sets whether the world 'camera' should follow the
+	 * <code>primaryRocket</code> horizontally.
+	 * @param centerOnRocketHorizontally whether the world camera should center
+	 * on the rocket horizontally
+	 */
 	public void setCenterOnRocketHorizontally(boolean centerOnRocketHorizontally) {
 		this.centerOnRocketHorizontally = centerOnRocketHorizontally;
 	}
 
+	/**
+	 * Returns true if the world 'camera' is currently following the 
+	 * <code>primaryRocket</code> vertically, and false otherwise
+	 * @return whether the world camera is centered on the rocket vertically
+	 */
 	public boolean centerOnRocketVertically() {
 		return centerOnRocketVertically;
 	}
 
+	/**
+	 * Sets whether the world 'camera' should follow the
+	 * <code>primaryRocket</code> vertically.
+	 * @param centerOnRocketVertically whether the world camera should center
+	 * on the rocket vertically
+	 */
 	public void setCenterOnRocketVertically(boolean centerOnRocketVertically) {
 		this.centerOnRocketVertically = centerOnRocketVertically;
 	}
 
+	/**
+	 * Returns whether the given Rocket is touching the ground, based on
+	 * y-coordinates only.
+	 * @param rocket the Rocket to check for a collision with the ground
+	 * @return true if the Rocket is touching the ground, false otherwise
+	 */
 	public boolean rocketTouchingGround(Rocket rocket) {
 		
 		return rocket.getY() + rocket.getHeight() >= getGroundY();
 		
 	}
 
+	/**
+	 * Gets the Rocket currently involved with simulation in this world.
+	 * @return this World's primary Rocket
+	 */
 	public Rocket getPrimaryRocket() {
 		return this.primaryRocket;
 	}
 
+	/**
+	 * Sets the Rocket currently involved with simulation in this world.
+	 * @param primaryRocket this World's new primary Rocket
+	 */
 	public void setPrimaryRocket(Rocket primaryRocket) {
 		this.primaryRocket = primaryRocket;
 	}
 
-	public ColorPalette getPalette() {
+	/**
+	 * Gets the color palette to use when drawing this World's objects
+	 * @return this World's color palette
+	 */
+	private ColorPalette getPalette() {
 		return this.palette;
 	}
 
+	/**
+	 * Sets the color palette to use when drawing this World's objects
+	 * @param palette this World's color palette
+	 */
 	public void setPalette(ColorPalette palette) {
 		this.palette = palette;
 		getMountainManager().setPalette(palette);
@@ -153,6 +248,11 @@ public class World {
 		
 	}
 	
+	/**
+	 * Fills the Canvas with a sky color. Other landscape elements, like the
+	 * ground and mountains, should be drawn over this background sky.
+	 * @param gc the GraphicsContext used to draw the World
+	 */
 	public void drawSky(GraphicsContext gc) {
 		
 		gc.setFill(getPalette().getSkyColor());
@@ -184,85 +284,57 @@ public class World {
 		
 	}
 	
+	/**
+	 * Translates the given GraphicsContext so that the Rocket appears in the
+	 * center of the window horizontally.
+	 * @param gc the GraphicsContext used to draw the World
+	 * @param center the Rocket to center the GraphicsContext on horizontally
+	 */
 	public void centerOnRocketHorizontally(GraphicsContext gc, Rocket center) {
 
-		// TODO After adding random terrain features, add code to
-		// make the camera just follow the Rocket horizontally
 		double xTranslate = centerOnRocketHorizontally() ? 
 				-center.getX() - gc.getTransform().getTx() 
 				+ getWindowWidth() / 2 : 0;
 		
-				/*
-		double yTranslate = centerOnRocketVertically()? 
-				-center.getY() + center.getHeight() / 2 - 
-				gc.getTransform().getTy() + getWindowHeight() / 2 : 0;
-		*/
 		gc.translate(xTranslate, 0);
 
 	}
 	
+	/**
+	 * Translates the given GraphicsContext so that the Rocket appears in the
+	 * center of the window vertically.
+	 * @param gc the GraphicsContext used to draw the World
+	 * @param center the Rocket to center the GraphicsContext on vertically
+	 */
 	public void centerOnRocketVertically(GraphicsContext gc, Rocket center) {
 
-		// TODO After adding random terrain features, add code to
-		// make the camera just follow the Rocket horizontally
-		/*
-		double xTranslate = centerOnRocketHorizontally() ? 
-				-center.getX() - gc.getTransform().getTx() 
-				+ getWindowWidth() / 2 : 0;*/
-		
 		double yTranslate = centerOnRocketVertically()? 
 				-center.getY() + center.getHeight() / 2 - 
 				gc.getTransform().getTy() + getWindowHeight() / 2 : 0;
+		
 		gc.translate(0, yTranslate);
 
 	}
 	
-	public void centerOnRocket(GraphicsContext gc, Rocket center) {
-
-		// TODO After adding random terrain features, add code to
-		// make the camera just follow the Rocket horizontally
-		
-		double xTranslate = centerOnRocketHorizontally() ? 
-				-center.getX() - gc.getTransform().getTx() 
-				+ getWindowWidth() / 2 : 0;
-		
-		double yTranslate = centerOnRocketVertically()? 
-				-center.getY() + center.getHeight() / 2 - 
-				gc.getTransform().getTy() + getWindowHeight() / 2 : 0;
-		
-		gc.translate(xTranslate, yTranslate);
-
-	}
-	
+	/**
+	 * Checks whether the GraphicsContext should follow the Rocket horizontally
+	 * or vertically and performs the necessary translations.
+	 * @param gc the GraphicsContext to translate
+	 */
 	public void alignGraphicsContext(GraphicsContext gc) {
 
-		if (centerOnRocketHorizontally() || centerOnRocketVertically()) {
+		if (centerOnRocketHorizontally()) {
 
-			for (Entity entity : getObjects()) {
-
-				if (entity.getClass() == Rocket.class || 
-						entity.getClass() == UserControlledRocket.class) {
-
-					Rocket rocket = (Rocket) entity;
-
-					if (centerOnRocketHorizontally()) {
-
-						centerOnRocketHorizontally(gc, rocket);
-
-					}
-
-					if (centerOnRocketVertically()) {
-
-						centerOnRocketVertically(gc, rocket);
-
-					}
-
-				}
-
-			}
+			centerOnRocketHorizontally(gc, getPrimaryRocket());
 
 		}
-		
+
+		if (centerOnRocketVertically()) {
+
+			centerOnRocketVertically(gc, getPrimaryRocket());
+
+		}		
+
 	}
 	
 	/**
