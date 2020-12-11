7c0bd436b6b4d1f4ef48d8d3009cc485ccba6fd3
unknown <parker.hutch@outlook.com>
2020-12-11 15:34:55 -0500

    cleanup and documentation

--- src/world/MountainManager.java
+++ src/world/MountainManager.java
@@ -6,19 +6,34 @@ import javafx.scene.paint.Color;
 
 import design.ColorPalette;
 
+/**
+ * A class that handles the creation and drawing of mountains, used in the 
+ * <code>World</code> class.
+ */
 public class MountainManager {
 	
 	private ArrayList<Double> mountainXPoints = new ArrayList<Double>();
 	private ArrayList<Double> mountainYPoints = new ArrayList<Double>();
 	
-	private double xStep;
-	private double maxShiftMagnitude;
+	private double xStep; 
+	private double maxShiftMagnitude; 
 	
 	private double groundY;
 	private double windowWidth;
 
 	private ColorPalette palette;
 
+	/**
+	 * Creates a MountainManager for a window of given width with arguments for
+	 * xStep, maxShiftMagnitude, the ground's top y-coordinate, and the color 
+	 * palette.
+	 * @param windowWidth the width of the Canvas the simulation takes place in
+	 * @param xStep how far apart mountain vertices should be
+	 * @param maxShiftMagnitude the maximum deviation between one mountain
+	 * vertex y-coordinate and the next
+	 * @param groundY the ground's top y-coordinate
+	 * @param palette the color palette to use when drawing the mountains
+	 */
 	public MountainManager(double windowWidth, double xStep, 
 			double maxShiftMagnitude, double groundY, ColorPalette palette) {
 		
@@ -27,46 +42,90 @@ public class MountainManager {
 		this.windowWidth = windowWidth;
 		this.groundY = groundY;
 		this.palette = palette;
-		fillPoints(windowWidth, 10);
+		fillPoints(20);
 		
 	}
 	
+	/**
+	 * Gets an ordered list of the mountain range's x-coordinates.
+	 * @return the mountain range's x-coordinates
+	 */
 	public ArrayList<Double> getMountainXPoints() {
 		return mountainXPoints;
 	}
 
+	/**
+	 * Gets an ordered list of the mountain range's y-coordinates.
+	 * @return the mountain range's y-coordinates
+	 */
 	public ArrayList<Double> getMountainYPoints() {
 		return mountainYPoints;
 	}
-
+	
+	/**
+	 * Gets the horizontal distance between mountain vertices.
+	 * @return the x step
+	 */
 	public double getxStep() {
 		return xStep;
 	}
 
+	/**
+	 * Sets the horizontal distance between mountain vertices.
+	 * @param xStep the new x step
+	 */
 	public void setxStep(double xStep) {
 		this.xStep = xStep;
 	}
 
+	/**
+	 * Gets the maximum deviation between one mountain vertex y-coordinate and 
+	 * the next
+	 * @return the maximum shift magnitude
+	 */
 	public double getMaxShiftMagnitude() {
 		return maxShiftMagnitude;
 	}
 
+	/**
+	 * Sets the maximum deviation between one mountain vertex y-coordinate and 
+	 * the next
+	 * @param maxShiftMagnitude the maximum shift magnitude
+	 */
 	public void setMaxShiftMagnitude(double maxShiftMagnitude) {
 		this.maxShiftMagnitude = maxShiftMagnitude;
 	}
 
+	/**
+	 * Gets the ground's top y-coordinate
+	 * @return the ground's top y-coordinate
+	 */
 	private double getGroundY() {
 		return groundY;
 	}
 
+	/**
+	 * Gets the width of the window that this MountainManager occupies.
+	 * @return the window width
+	 */
 	private double getWindowWidth() {
 		return windowWidth;
 	}
 
+	/**
+	 * Sets the width of the window that this MountainManager occupies.
+	 * @param windowWidth the window width
+	 */
 	public void setWindowWidth(double windowWidth) {
 		this.windowWidth = windowWidth;
 	}
 
+	/**
+	 * Generates the y-coordinate of the next mountain vertex based on the last
+	 * one.
+	 * @param lastY the last mountain vertex's y-coordinate
+	 * @return the next mountain vertex's y-coordinate
+	 */
 	private double getNextY(double lastY) {
 		
 		return (lastY - maxShiftMagnitude) + 
@@ -74,25 +133,28 @@ public class MountainManager {
 		
 	}
 
-	public ColorPalette getPalette() {
+	/**
+	 * Gets the color palette to use when drawing the mountains.
+	 * @return the MountainManager's color palette
+	 */
+	private ColorPalette getPalette() {
 		return this.palette;
 	}
 
+	/**
+	 * Sets the color palette to use when drawing the mountains
+	 * @param palette the MountainManager's color palette
+	 */
 	public void setPalette(ColorPalette palette) {
 		this.palette = palette;
 	}
 	
-	private void fillPoints(double windowWidth, int initialGenSize) {
-		
-		//int arrayLength = (int) ((windowWidth / xStep) * coefficient);
-		//setMountainXPoints(new double[arrayLength]);
-		//setMountainYPoints(new double[arrayLength]);
-		
-		// TODO don't forget to make sure that mountainXPoints starts and ends with 0
-		// TODO and that mountainYPoints starts and ends with groundY
-		// TODO Maybe ignore this when generating the points, and use 
-		// TODO variables like actualXPoints where the neccessary ones have been
-		// inserted at beginning and end of the arrays when returning the polygon?
+	/**
+	 * Fills the lists storing mountain x and y points by continuously 
+	 * generating vertex x and y coordinates.
+	 * @param initialGenSize the number of mountain vertices to generate
+	 */
+	private void fillPoints(int initialGenSize) {
 		
 		for (int i = 0; i < initialGenSize; i ++) {
 			
@@ -120,9 +182,16 @@ public class MountainManager {
 		
 	}
 	
+	/**
+	 * Makes sure that there are enough mountain vertices to fill the Canvas
+	 * that the MountainManager occupies.
+	 * @param canvasLeftX the left x-coordinate of the Canvas used to draw the 
+	 * mountains
+	 * @param margin the area outside of the Canvas to generate mountain 
+	 * vertices in
+	 */
 	private void fillViewingWindow(double canvasLeftX, double margin) {
 		if (getMountainXPoints().size() == 0 || getMountainYPoints().size() == 0) {
-			
 			
 			System.out.println("still loading mountains");
 
@@ -136,15 +205,6 @@ public class MountainManager {
 				double leftMountainY = getMountainYPoints().get(0);
 				getMountainYPoints().add(0, getNextY(leftMountainY));
 				
-				//double leftMountainX = getMountainXPoints()[0];
-				//double leftMountainY = getMountainYPoints()[0];
-				
-				
-				//setMountainXPoints(insertAtBeginning(getMountainXPoints(), leftMountainX - xStep));
-				//setMountainYPoints(insertAtBeginning(getMountainYPoints(), getNextY(leftMountainY)));
-
-				// TODO Call fillViewingWindow again(recursion?)
-
 			}
 
 			double canvasRightX = canvasLeftX + getWindowWidth();
@@ -155,16 +215,17 @@ public class MountainManager {
 				
 				getMountainXPoints().add(rightMountainX + xStep);
 				getMountainYPoints().add(getNextY(rightMountainY));
-				//setMountainXPoints(append(getMountainXPoints(), rightMountainX + xStep));
-				//setMountainYPoints(append(getMountainYPoints(), getNextY(rightMountainY)));
 
 			}
 
 		}
 		
-		
 	}
 	
+	/**
+	 * Draws mountains on a Canvas with the given GraphicsContext.
+	 * @param gc the GraphicsContext to use when drawing the mountains
+	 */
 	public void draw(GraphicsContext gc) {
 		
 		fillViewingWindow(-gc.getTransform().getTx(), 500);
@@ -197,7 +258,6 @@ public class MountainManager {
 			gc.setLineWidth(3);
 			gc.strokeLine(getMountainXPoints().get(i), getMountainYPoints().get(i), 
 					getMountainXPoints().get(i+1), getMountainYPoints().get(i+1));
-			
 			
 		}
 		
